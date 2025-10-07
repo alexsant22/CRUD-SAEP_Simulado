0025ce556f38f46e0115c38e0414bf192bf56977
Alexandre Santos <alexandre.santana2201@gmail.com>
2025-10-07 13:47:12 -0300

    Projeto Criado

---
+++ saepSimulado/src/main/java/com/example/saepSimulado/SaepSimuladoApplication.java
@@ -0,0 +1,13 @@
+package com.example.saepSimulado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaepSimuladoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaepSimuladoApplication.class, args);
+	}
+
+}
---
+++ saepSimulado/src/test/java/com/example/saepSimulado/SaepSimuladoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saepSimulado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaepSimuladoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
