2c316d1e714d2ad16287a20f2eafc4aa36184927
Alexandre Santos <alexandre.santana2201@gmail.com>
2025-10-07 15:08:37 -0300

    Config Cors

---
+++ saepSimulado/src/main/java/com/example/saepSimulado/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.saepSimulado.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permite qualquer origem com credenciais
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
